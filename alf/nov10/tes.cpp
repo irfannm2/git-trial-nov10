980ec987711b22eebc3f8ad0442452d963049af0
irfannm2 <muhammad.irfan2503@mail.ugm.ac.id>
2022-11-10 21:07:49 +0700

    Revert "perubahan E"
    This reverts commit 105f468cfa6b3b292efd759f289aa10df9070800.

--- alf/nov10/tes.cpp
+++ alf/nov10/tes.cpp
@@ -7,18 +7,5 @@ int main()
     cout<<"\nA";
     cout<<"\nB";
     cout<<"\nC";
-    cout<<"\nD";
-    cout<<"\nE";
-    cout<<"\nF";
-    cout<<"\nG";
-    cout<<"\nH";
-    cout<<"\nI";
-    cout<<"\nJ";
-    cout<<"\nK";
-    cout<<"\nL";
-    cout<<"\nM";
-    cout<<"\nN";
-    cout<<"\nO";
-    cout<<"\nP";
     return 0;
 }
